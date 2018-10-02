446cf7d6d6230f05c2af7193a36b23fa978cb09a
Martín <tinchobeltrame@gmail.com>
2018-10-02 11:35:18 -0300

    Refactor

--- slider_md/src/main/java/ar/edu/untref/dyasc/entrada/LectorArchivos.java
+++ slider_md/src/main/java/ar/edu/untref/dyasc/entrada/LectorArchivos.java
@@ -8,9 +8,11 @@ import java.util.stream.Stream;
 
 public class LectorArchivos {
 
+	private static final String RUTA_UBICACION_ARCHIVOS = "../documentos/";
+
 	public String obtenerNombre(String nombreArchivo) {
 
-		File file = new File("../documentos/" + nombreArchivo);
+		File file = new File(RUTA_UBICACION_ARCHIVOS + nombreArchivo);
 
 		if (file.exists() && !file.isDirectory()) {
 			String nombreConExtension = file.getName();
@@ -27,7 +29,7 @@ public class LectorArchivos {
 
 		StringBuilder contenido = new StringBuilder();
 
-		try (Stream<String> lineas = Files.lines(Paths.get("../documentos/" + nombreArchivo))) {
+		try (Stream<String> lineas = Files.lines(Paths.get(RUTA_UBICACION_ARCHIVOS + nombreArchivo))) {
 			lineas.forEach(linea -> contenido.append(linea).append("\n"));
 		} catch (IOException e) {
 			e.printStackTrace();
