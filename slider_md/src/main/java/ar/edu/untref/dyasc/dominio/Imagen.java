4bb83603106ed7b0b8d30f3d2bb1686ad6a0faf2
Martín <tinchobeltrame@gmail.com>
2018-10-08 15:34:28 -0300

    Componente Imagen implementado

--- slider_md/src/main/java/ar/edu/untref/dyasc/dominio/Imagen.java
+++ slider_md/src/main/java/ar/edu/untref/dyasc/dominio/Imagen.java
@@ -3,16 +3,26 @@ package ar.edu.untref.dyasc.dominio;
 public class Imagen extends Componente {
 
 	private String componenteActual;
+	private Componente siguienteComponente;
 
 	public Imagen(Componente siguienteComponente, String componenteActual) {
 		super(siguienteComponente, componenteActual);
+
+		this.siguienteComponente = siguienteComponente;
 		this.componenteActual = componenteActual;
 	}
 
 	@Override
 	void parsearMarkdown() {
-		String textoComponente = this.componenteActual.substring(2);
-		String nuevoContenido = "<img src=\"" + textoComponente + "\"/>";
-		agregarNuevoContenido(nuevoContenido);
+
+		String etiqueta = this.componenteActual.substring(0, 2);
+
+		if (etiqueta.equals("i:")) {
+			String textoComponente = this.componenteActual.substring(2);
+			String nuevoContenido = "<img src=\"" + textoComponente + "\"/>";
+			agregarNuevoContenido(nuevoContenido);
+		} else {
+			this.siguienteComponente.parsearMarkdown();
+		}
 	}
 }
--- slider_md/src/test/java/ar/edu/untref/dyasc/dominio/ComponenteImagenDebe.java
+++ slider_md/src/test/java/ar/edu/untref/dyasc/dominio/ComponenteImagenDebe.java
@@ -2,11 +2,19 @@ package ar.edu.untref.dyasc.dominio;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ComponenteImagenDebe {
 
 	private static final String COMPONENTE_IMAGEN = "i:imagen.png";
 	private static final String OTRO_COMPONENTE_IMAGEN = "i:otra_imagen.png";
+	private static final String COMPONENTE_H1 = "# Titulo h1";
+
+	@Mock Componente componenteH1;
 
 	private Imagen componenteImagen;
 
@@ -32,5 +40,14 @@ public class ComponenteImagenDebe {
 
 		String esperado = "<img src=\"otra_imagen.png\"/>";
 		Assert.assertEquals(esperado, obtenido);
+	}
+
+	@Test
+	public void verificar_que_se_llama_a_otro_componente_cuando_la_etiqueta_no_corresponde_a_h1() {
+
+		componenteImagen = new Imagen(componenteH1, COMPONENTE_H1);
+		componenteImagen.parsearMarkdown();
+
+		Mockito.verify(componenteH1).parsearMarkdown();
 	}
 }
