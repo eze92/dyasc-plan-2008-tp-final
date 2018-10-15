daec971d57bfe0af51ec64f1e09891417eb87149
Martín <tinchobeltrame@gmail.com>
2018-10-15 17:40:05 -0300

    Cambio de nombre

--- slider_md/src/test/java/ar/edu/untref/dyasc/dominio/ServicioProgramaDebe.java
+++ slider_md/src/test/java/ar/edu/untref/dyasc/dominio/ServicioProgramaDebe.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ServicioEntradaDebe {
+public class ServicioProgramaDebe {
 
 	private static final String SECCION = "---";
 	private static final String H1 = "# titulo";
@@ -13,24 +13,15 @@ public class ServicioEntradaDebe {
 	private static final String ARCHIVO = "---\n# titulo";
 
 	private Contexto contexto;
-	private ServicioEntrada servicioEntrada;
-
-	private Componente seccion;
+	private ServicioPrograma servicioEntrada;
 
 	@Before
 	public void inicializar() {
 
 		contexto = new Contexto();
 
-		servicioEntrada = new ServicioEntrada(contexto);
+		servicioEntrada = new ServicioPrograma(contexto);
 		servicioEntrada.crearContenido(ARCHIVO);
-
-		FinSeccion finSeccion = new FinSeccion(null, contexto);
-		Imagen imagen = new Imagen(finSeccion, contexto);
-		ItemLista lista = new ItemLista(imagen, contexto);
-		TituloH2 tituloH2 = new TituloH2(lista, contexto);
-		TituloH1 tituloH1 = new TituloH1(tituloH2, contexto);
-		seccion = new Seccion(tituloH1, contexto);
 	}
 
 	@Test
@@ -45,7 +36,7 @@ public class ServicioEntradaDebe {
 	@Test
 	public void generar_la_salida_en_formato_string() {
 
-		String obtenido = servicioEntrada.obtenerSalida(seccion);
+		String obtenido = servicioEntrada.obtenerSalida();
 
 		String esperado = "<section>" + "<h1>titulo</h1>" + "</section>";
 		Assert.assertEquals(esperado, obtenido);
