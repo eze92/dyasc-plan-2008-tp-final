85b5778fa81243d110947b4d0b6b46d76d16861d
Martín <tinchobeltrame@gmail.com>
2018-10-16 11:06:44 -0300

    Se eliminan archivos basura

--- slider_md/carpetaPruebaCopia/carpetaPruebaCopia/src/main/java/ar/edu/untref/dyasc/salida/Salida.java
+++
@@ -1,8 +0,0 @@
-package ar.edu.untref.dyasc.salida;
-
-import java.io.IOException;
-
-public abstract class Salida {
-
-	public abstract void imprimir(String contenidoSalida) throws IOException, NoExisteDirectorioException;
-}
