4a9453bda2471b0d148a2cf59d46ac14e023382d
Ezequiel <ezequiel.ledesma026@gmail.com>
2018-10-15 21:27:52 -0300

    se elimina carpeta basura

--- slider_md/carpetaPruebaCopia/src/main/java/ar/edu/untref/dyasc/salida/SalidaPantalla.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.untref.dyasc.salida;
-
-public class SalidaPantalla extends Salida {
-
-	@Override
-	public void imprimir(String contenidoSalida) {
-		System.out.println(contenidoSalida);
-	}
-}
